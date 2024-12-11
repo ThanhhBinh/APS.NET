c83159c45eaf2612ea66ce058d106f20871062c8
HugoPhan0501 <hugo@nautilus.solutions>
2024-12-11 18:33:56 +0700

    Update connect db

--- WebASP/Areas/Admin/Controllers/HomeAdminController.cs
+++ WebASP/Areas/Admin/Controllers/HomeAdminController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace WebASP.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class HomeAdminController : Controller
     {
         // GET: Admin/Home
         public ActionResult Index()
